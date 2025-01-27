bdda126e13ab041c424984df6407c8c1ba416f04
Serdar Geldyev <serdarabad@gmail.com>
2025-01-27 23:43:27 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react';
 
 import reactLogo from './assets/react.svg';
-
-import viteLogo from '/vite.svg';
+import viteLogo from './assets/vite.svg';
 
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
 
   return (
     <>
