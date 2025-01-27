fa5235324c72c3d02b6a9e58b72f11dd5d7802f1
Serdar Geldyev <serdarabad@gmail.com>
2025-01-27 23:48:06 +0300

    husky is working

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import viteLogo from './assets/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -15,7 +15,7 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react" alt="Reacto logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
