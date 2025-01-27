938f7346fce77a5ecfdec9d219d9b47f7e174fd3
Serdar Geldyev <serdarabad@gmail.com>
2025-01-27 23:35:22 +0300

    linter is working

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,56 +1,60 @@
+/* eslint-disable prettier/prettier */
 module.exports = {
   env: {
     browser: true,
     es2021: true,
   },
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: '@babel/eslint-parser',
     requireConfigFile: true,
   },
   extends: [
-    "airbnb",
-    "airbnb/hooks",
-    "plugin:react/recommended",
-    "eslint:recommended",
-    "prettier",
-    "plugin:prettier/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'eslint:recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
   ],
   rules: {
-    indent: ["error", 2],
-    "prettier/prettier": "error",
-    "linebreak-style": [0, "unix"],
-    quotes: ["error", "single"],
-    semi: ["error", "always"],
-    "react/react-in-jsx-scope": 0,
-    "react/prop-types": 0,
-    "import/no-unresolved": [2, { caseSensitive: false }],
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
-    "import/order": [
+    indent: ['error', 2],
+    'prettier/prettier': 'error',
+    'linebreak-style': [0, 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'react/react-in-jsx-scope': 0,
+    'react/prop-types': 0,
+    'import/no-absolute-path': 0,
+    'import/extensions': 0,
+    'import/no-unresolved': [2, { caseSensitive: false }],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'import/order': [
       2,
       {
-        groups: [
-          "builtin",
-          "external",
-          "internal",
-          "parent",
-          "sibling",
-          "index",
-        ],
-        "newlines-between": "always",
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
       },
     ],
   },
+  ignorePatterns: ['node_modules/', 'dist/', 'build/', 'coverage/', '.vscode/', 'vite.config.js'],
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-        moduleDirectory: ["node_modules", "src/"],
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.svg'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+      alias: {
+        map: [
+          ['@/', './src'],
+          ['', './public'],
+        ],
+        extensions: ['.js', '.jsx', '.svg'],
       },
     },
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
 };
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
+import path from 'path';
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  alias: {
+    '/public': path.resolve(__dirname, 'public'),
+  },
 });
