86cb3d13b917840c06c4b3f31b9eb4f64082586d
Serdar Geldyev <serdarabad@gmail.com>
2025-01-30 17:18:30 +0300

    added check for empty return string

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-nested-ternary */
 /* eslint-disable react/jsx-no-useless-fragment */
+/* eslint-disable no-console */
 import { useState, useEffect } from 'react';
 import { LoadingOutlined } from '@ant-design/icons';
 import { Tag, Col, Spin } from 'antd';
@@ -10,9 +11,9 @@ import MovieFetcher from '../../services/MovieFetcher.js';
 import './MovieCard.css';
 
 function MovieCard() {
-  const [dataY, setData] = useState([]); // Initialize as an array
-  const [loading, setLoading] = useState(true); // State to track loading status
-  const [error, setError] = useState(null); // State to track any errors
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const query = 'return';
   useEffect(() => {
@@ -20,13 +21,12 @@ function MovieCard() {
       try {
         const data = await MovieFetcher.getData(query);
         if (Array.isArray(data)) {
-          setData(data); // Ensure we're setting an array
+          setMovies(data);
         } else {
           throw new Error('Fetched data is not an array');
         }
       } catch (e) {
         setError(e.message); // Set error state
-        // eslint-disable-next-line no-console
         console.error('Error fetching movies:', e); // Log the error
       } finally {
         setLoading(false); // Stop loading
@@ -34,7 +34,7 @@ function MovieCard() {
     };
 
     updateTitle();
-  }, [query, setError, setData, setLoading]); // Empty dependency array for running once on mount
+  }, [query, setError, setMovies, setLoading]);
 
   function truncateString(str, max = 200) {
     if (str.length <= max) return str;
@@ -57,7 +57,7 @@ function MovieCard() {
       ) : error ? (
         <h1>Error: {error}</h1>
       ) : (
-        dataY.map((dataX) => {
+        movies.map((dataX) => {
           try {
             const formattedReleaseDate = format(dataX.release_date, 'MMMM dd, yyyy');
             return (
@@ -81,7 +81,6 @@ function MovieCard() {
               </Col>
             );
           } catch (e) {
-            // eslint-disable-next-line no-console
             console.error('Error formatting date:', e);
             return (
               <Col md={12} sm={24} key={dataX.id}>
